82815593b041148b7bfa702d8cb9454158de696b
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2022-11-03 17:17:47 -0400

    forms

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+let form = document.getElementsByTagName("form")[0]
+let addButton = document.getElementById("Add");
+let inputs = document.querySelectorAll(".entry>input, .entry>textarea, select" );
+alert(inputs.length);   
+inputs.forEach(input => {
+        console.log(input)
+    input.addEventListener("change", formValidation);
+})
+
+function formValidation(){
+    if(!form.valid)
+    {
+        addButton.disabled =true;
+        addButton.style.backgroundColor = "grey";
+    }
+}
 //______________________________________________
