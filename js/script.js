d76428dd276071cc71550c8f7d890d62010a3a62
dxxxxy <introsdream@gmail.com>
2022-11-04 11:54:11 -0400

    implemented [{}] ->table

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 let form = document.getElementsByTagName("form")[0]
 let addButton = document.getElementById("Add");
-let inputs = document.querySelectorAll(".entry>input, .entry>textarea, select" );
-alert(inputs.length);   
+let inputs = document.querySelectorAll(".entry>input, .entry>textarea, select");
+alert(inputs.length);
 inputs.forEach(input => {
-        console.log(input)
+    console.log(input)
     input.addEventListener("change", formValidation);
 })
 
-function formValidation(){
-    if(!form.valid)
-    {
-        addButton.disabled =true;
+function formValidation() {
+    if (!form.valid) {
+        addButton.disabled = true;
         addButton.style.backgroundColor = "grey";
     }
 }
@@ -19,9 +18,13 @@ function formValidation(){
 //sample project array
 const projects = [{
     proj_id: 1,
-    name: "John",
-    age: 30,
-    city: "New York"
+    owner: "John",
+    title: "Project 1",
+    category: "Category 1",
+    hours: 10,
+    rate: 100,
+    status: "In Progress",
+    description: "Simple description",
 }]
 
 const writeLocal = () => {
@@ -52,12 +55,36 @@ const clearLocal = () => {
 
 const loadLocal = () => {
     const local = JSON.parse(localStorage.getItem("data"))
+    if (!local) return updateTable()
 
     //load only new projects
     let toLoad = local.filter(project => !projects.find(proj => proj.proj_id === project.proj_id))
     projects.push(...toLoad)
 
     console.log("loaded", projects)
+
+    updateTable()
+}
+
+const updateTable = () => {
+    //get table
+    const table = document.querySelector("tbody")
+
+    //set table to have projects rows
+    table.innerHTML = projects.map(project => {
+        return `<tr>
+                    <td>${project.proj_id}</td>
+                    <td>${project.owner}</td>
+                    <td>${project.title}</td>
+                    <td>${project.category}</td>
+                    <td>${project.hours}</td>
+                    <td>${project.rate}</td>
+                    <td>${project.status}</td>
+                    <td>${project.description}</td>
+                    <td><img class="edit" src="images/edit.png" alt="edit" /></td>
+                    <td><img class="trash" src="images/trash.png" alt="trash" /></td>
+                </tr>`
+    }).join("")
 }
 
 const write = document.querySelector("#write")
